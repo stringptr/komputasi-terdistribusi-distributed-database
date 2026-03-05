d69ebb9a942380b4576aaa71da16139837acae17
stringptr <satriamanggala@student.uns.ac.id>
2026-03-05 18:32:41 +0700

    fix(dependency): @yugabytedb/pg cannot be found

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactCompiler: true,
-  serverExternalPackages: ['@yugabytedb/pg', 'pg'],
+  transpilePackages: ['@yugabytedb/pg', 'pg'],
 };
 
 export default nextConfig;
